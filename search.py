95d748d94c565b3a16625149c3a546eb2b24f6bf
Sarina Xie <sx2166@columbia.edu>
2017-09-24 02:11:52 -0400

    res is a dict :////////////////

---
+++ search.py
@@ -0,0 +1,15 @@
+from apiclient.discovery import build
+import json
+import sys 		#to take command line args
+
+import pprint
+
+service = build("customsearch", "v1", developerKey="AIzaSyBbGfil_xv2ICSW4xjT5RYY92l96nahFEs")
+
+res = service.cse().list(
+	q='cow',
+	cx='007382945159574133954:avqdfgjg420',
+	).execute()
+
+
+pprint.pprint(res['items'][0]['formattedUrl'])
