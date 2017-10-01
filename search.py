7daa7a96088c4b554c60a9fee40671e534a85dd8
Sarina Xie <sx2166@columbia.edu>
2017-10-01 13:51:40 -0400

    while loop logic

--- search.py
+++ search.py
@@ -11,29 +11,44 @@ def removeStopWords(tokenList):
 
 
 
-def findFrequency():
+def findFrequency(alist):
+	alist = ['chicken']
+	return alist
 
+def sendQuery(service, q):
+	res = service.cse().list(
+		q=q,
+		cx='007382945159574133954:avqdfgjg420',
+		).execute()
+	return res
 
 def main():
 	service = build("customsearch", "v1", developerKey="AIzaSyBbGfil_xv2ICSW4xjT5RYY92l96nahFEs")
+	
+	goalP = float(sys.argv[2]) * 10
+	p = -1
+	q = sys.argv[3]
+	res = sendQuery(service, q)
+	while p < goalP and p != 0:
+		print(q)
+		titles = []
+		snippets = []
+		for page in res['items']:
+			tit = page['htmlTitle'].encode("utf-8")
+			snip = page['htmlSnippet'].encode("utf-8")
+			print(tit + "\n" + snip)
+			mark = raw_input('relevant, y/n?')
+			if mark == 'y':
+				titles.append(tit.lower)
+				snippets.append(snip.lower)
 
-	res = service.cse().list(
-		q='cow',
-		cx='007382945159574133954:avqdfgjg420',
-		).execute()
+		p = len(titles)
 
-	title = res['items'][0]['htmlTitle'].lower()
-	summary = res['items'][0]['htmlSnippet'].lower()
+		newWords = findFrequency(titles)
+		for word in newWords:
+			q = q + " " + word
 
-	titleTokens = removeStopWords(re.findall(r'\b[a-z]{3,20}\b', title))
-	summaryTokens = removeStopWords(re.findall(r'\b[a-z]{3,20}\b', summary))
-#	print titleTokens
-#	pprint.pprint(res['items'][0]['htmlTitle'])
-#	pprint.pprint(res['items'][0]['formattedUrl'])
-#	pprint.pprint(res['items'][0]['htmlSnippet'])
-#	pprint.pprint(res['items'])
-
-
-
+		res = sendQuery(service, q)
+	print("done")
 
 if __name__ == '__main__': main()
